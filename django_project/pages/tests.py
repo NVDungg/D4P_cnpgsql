a364eba5470e72dd27924fa0b9b22d3d41efa5e0
NVDungg <dung692000@gmail.com>
2022-12-14 22:08:58 +0700

    Testcase

--- django_project/pages/tests.py
+++ django_project/pages/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase, SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
 # Create your tests here.
 
 class HomepageTerst(SimpleTestCase):
