b54fb02bfb36bfb5472e8bfea5d543590c27f366
NVDungg <dung692000@gmail.com>
2022-12-21 08:55:16 +0700

    user auth login/out

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django Admin
     path('admin/', admin.site.urls),
+    #User management
+    path("accounts/", include("django.contrib.auth.urls")),
+    # Local apps
     path("", include("pages.urls")),
 ]
