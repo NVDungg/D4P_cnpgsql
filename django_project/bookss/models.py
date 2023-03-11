3fea775a77b8c38d88604ab36b16da2c2643fcf1
NVDungg <dung692000@gmail.com>
2023-03-11 15:45:06 +0700

    add new table Review in bookss app had foreignkey with Book and get user model

--- django_project/bookss/models.py
+++ django_project/bookss/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Book(models.Model):
@@ -15,3 +16,8 @@ class Book(models.Model):
     
     def __str__(self):
         return self.title
+    
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="reviews")
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
