3d1bd464aa0347b1ff209555b3f22d414c4c17fa
NVDungg <dung692000@gmail.com>
2023-03-14 16:13:04 +0700

    Creat search form, function in view n url

--- django_project/bookss/views.py
+++ django_project/bookss/views.py
@@ -1,4 +1,5 @@
 from .models import Book
+from django.db.models import Q
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin, PermissionRequiredMixin
 # Create your views here.
@@ -14,3 +15,14 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = "books/book_detail.html"
     login_url = "account_login"
     permission_required = ("books.special_status", )
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = "book_list"
+    template_name = "books/search_results.html"
+
+    def get_queryset(self): # new
+        query = self.request.GET.get("q")
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
